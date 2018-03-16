acc9d9ff10b0bc5290aec70d71e38421a1844981
laraismael1 <laraismael123@gmail.com>
2018-03-16 09:27:04 -0700

    fullstach object

---
+++ jsObj.js
@@ -0,0 +1,67 @@
+* concepts: problem/desire descriptions
+  & methodologies: solution descriptions
+    % technologies: hardware | software
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+* Information Storage
+  & file systems
+  & hard drive
+  & flash drive
+  & memory
+  & relational databases
+    % MySQL
+    % Postgres
+  & non-relational database
+    % Mongo DB
+    % Dynamo DB
+
+* Information Processing
+  & Operating Systems
+    % Linux
+    % Windows
+    % Mac OS
+    % IOS
+    % Android
+  & Computer Languages (parsing, interpreting, executing code)
+    % Javascript (spidermonkey, webkit, v8)
+    % Python
+    % Ruby
+  & Frameworks & Libraries
+    % Node 
+    % Mongoose #mongodb
+    % React / Redux
+    % Backbone
+    % Angular
+    % MORE
+  & Web Services
+    % AWS
+    % Heroku
+    % Git Hub
+  & Tool Sets
+    % Webpack
+    % NPM
+  & Content Management Systems CMS
+    % Joomla
+    % Wordpress
+    % Drupal
+  & Full Stack Architectures
+    % LAMP
+    % MEAN
+
+* Information Sharing
+  & Telecommunication
+    % Satelites 
+    % Cellular Networks
+    % Cables
+    % Network Cards
+    % WiFi
+  & Protocols
+    % TCP/IP Stack
+      %% HTTP
+        %%% Web Browsers
+        %%% Web Servers
+
+* Information Experience
+  & API
+  & UI
+
+
